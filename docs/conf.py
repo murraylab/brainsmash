9511f895aa873b971eb703c9086b479943812765
Joshua Burt <joshua.burt@yale.edu>
2020-11-09 15:34:06 -0500

    debugging docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
